221ad57c5f205dc29fae5afa465f580d49046da9
169398 <kulubiidris@gmail.com>
2024-02-06 09:51:15 +0300

    first commit

---
+++ Idris12/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Providers from "./Provider";
+
+export const metadata: Metadata = {
+  title: "smartly",
+  description: "...",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="">
+        <Providers
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ Idris12/app/page.tsx
@@ -0,0 +1,8 @@
+import AuthComponent from "@/components/Other/AuthComponent";
+
+
+export default function Home() {
+  return (
+    <AuthComponent/>
+  )
+}
